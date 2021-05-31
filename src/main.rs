a55915d8969890351f6dee834dd316a764f9f467
sebastian <sebsch@geblubber.org>
2021-05-31 20:20:38 +0200

    refactor: merge the two methods into one (yagnify)

--- src/main.rs
+++ src/main.rs
@@ -6,20 +6,19 @@ use regex::Regex;
 
 mod reader;
 
-fn pattern_indices(re: &Regex, text: &str) -> Vec<usize> {
-    re.find_iter(text).map(|m| m.start()).collect()
-}
 
-fn split_text(mut idxs: Vec<usize>, text: &str) -> Vec<&str> {
+fn split_text<'a>(separator: &Regex, text: &'a str) -> Vec<&'a str> {
     let mut slices = Vec::new();
     let mut lhs: usize = 0;
-    if idxs.last().unwrap() != &text.len() {
-        idxs.insert(idxs.len(), text.len())
+
+    let mut indices: Vec<usize> = separator.find_iter(text).map(|m| m.start()).collect();
+    if indices.last().unwrap() != &text.len() {
+        indices.insert(indices.len(), text.len())
     }
 
-    for i in idxs {
-        slices.push(&text[lhs..i]);
-        lhs = i;
+    for rhs in indices {
+        slices.push(&text[lhs..rhs]);
+        lhs = rhs;
     }
 
     slices
@@ -34,26 +33,9 @@ mod tests {
     use super::*;
 
     #[test]
-    fn test_newline_indexer() {
-        let rgx = Regex::new(r"(?m)^\[").unwrap();
-        assert_eq!(pattern_indices(&rgx, "foo\n[bar"), [4]);
-        assert_eq!(pattern_indices(&rgx, "[foo\nbar"), [0]);
-        assert_eq!(pattern_indices(&rgx, "[foo\n[bar"), [0, 5]);
-        assert_eq!(pattern_indices(&rgx, "foo\nbar"), [])
-    }
-
-    #[test]
-    fn test_split_text() {
-        let ids: Vec<usize> = vec![1, 4, 5];
-        assert_eq!(split_text(ids, "Mary had a little lamb"),
-                   vec!["M", "ary", " ", "had a little lamb"])
-    }
-
-    #[test]
     fn test_combination() {
-        let rgx = Regex::new(r"(?m)^\[").unwrap();
+        let separator = Regex::new(r"(?m)^\[").unwrap();
         let text = "Mary \n[had a \nlittle lamb";
-        let idxs = pattern_indices(&rgx, &text);
-        assert_eq!(split_text(idxs, &text), vec!["Mary \n", "[had a \nlittle lamb"])
+        assert_eq!(split_text(&separator, &text), vec!["Mary \n", "[had a \nlittle lamb"])
     }
 }
