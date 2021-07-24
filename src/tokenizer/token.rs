6a8c5e310b4039667ae2f744d7f7f93a274f4eb7
sebastian <sebsch@geblubber.org>
2021-07-24 08:28:50 +0200

    Add a bunch of todos for further progress

--- src/tokenizer/token.rs
+++ src/tokenizer/token.rs
@@ -1,9 +1,11 @@
 #[derive(Debug, Clone)]
 pub struct LogLineToken {
-    pub raw_data: String, // TODO: gathered_timestamp
-                          // TODO: indices: vec[&str]
-                          // TODO: log_source: &str
-                          // TODO: log_timestamp  -> if possible extract from data; default on gathered_timestamp
+    // TODO: gathered_timestamp
+    // TODO: indices: vec[&str]
+    // TODO: log_source: &str
+    // TODO: service_name: &str
+    // TODO: log_timestamp  -> if possible extract from data; default on gathered_timestamp
+    pub raw_data: String,
 }
 
 impl LogLineToken {
