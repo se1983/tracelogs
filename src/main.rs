882c8eff77e79163a47526f391cb33fed8e9e6d5
sebastian <sebsch@geblubber.org>
2021-07-24 06:56:19 +0200

    improve error handling during file reading.

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,8 @@
-use tracelogs::tokenizer::LogLineTokenizer;
-use tokio::fs;
-use std::time::Duration;
 use regex::Regex;
+use std::time::Duration;
+use tokio::fs;
 use tokio::time::sleep;
+use tracelogs::tokenizer::LogLineTokenizer;
 
 struct LogFileAdapter {
     file_path: String,
@@ -11,34 +11,45 @@ struct LogFileAdapter {
 
 impl LogFileAdapter {
     pub fn new(file_path: String, tokenizer: LogLineTokenizer) -> Self {
-        LogFileAdapter { file_path, tokenizer }
+        LogFileAdapter {
+            file_path,
+            tokenizer,
+        }
+    }
+
+    async fn next(&mut self) -> Result<(), tokio::io::Error> {
+        let file_cont = fs::read_to_string(&self.file_path).await?;
+
+        for (i, line) in file_cont.lines().enumerate() {
+            if i < self.tokenizer.count {
+                continue;
+            }
+            let line = "\n".to_owned() + line;
+            self.tokenizer.push(&line);
+        }
+
+        Ok(())
     }
 
     pub async fn watch(&mut self) {
         loop {
-            sleep(Duration::from_millis(300)).await;
-
-            let contents = fs::read_to_string(&self.file_path).await.unwrap();
-
-            for (i, line) in contents.lines().enumerate() {
-                if i < self.tokenizer.count { continue; }
-                let line = "\n".to_owned() + line;
-                self.tokenizer.push(&line);
+            match self.next().await {
+                Ok(_) => sleep(Duration::from_millis(300)).await,
+                Err(err) => {
+                    eprintln!("error open file {}  [{}]", self.file_path, err);
+                    sleep(Duration::from_millis(3000)).await;
+                }
             }
         }
     }
 }
 
-
 async fn run() {
-    let newline_rgx = Regex::new(r"(?m)^").unwrap();
+    let newline_rgx = Regex::new(r"(?m)\n^\[").unwrap();
     let tokenizer = LogLineTokenizer::new(newline_rgx);
-    let mut reader = LogFileAdapter::new(
-        String::from("/tmp/test.txt"),
-        tokenizer);
+    let mut reader = LogFileAdapter::new(String::from("/var/log/Xorg.0.log"), tokenizer);
     reader.watch().await;
 }
-
 
 #[tokio::main]
 async fn main() {
